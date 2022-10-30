ea41ac47157994d1aa6c39f286805782f93b3c57
CanarDev <vincent.michel@edu.devinci.Fr>
2022-10-31 00:15:33 +0100

    :rocket: First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'discord': '#5865f2',
+        'linkedin': '#0e76a8',
+        'github': '#171515',
+        'instagram': '#C32AA3',
+        'twitter': '#1DA1F2'
+      }
+    },
+  },
+  plugins: [],
+}
